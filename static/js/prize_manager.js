deb9b76e35b697beaf4e87eb5eb23fd3c4f97320
yejin <banana2990@outlook.com>
2026-06-21 02:52:53 +0900

    20260621 화면 수정

--- static/js/prize_manager.js
+++ static/js/prize_manager.js
@@ -4,12 +4,12 @@
 const PrizeManager = {
     // 1) 원천 데이터 마스터 테이블 (한 곳에서만 수정하면 전체 반영)
     prizes: [
-        { condition: (score) => score === 100, name: "👑 명예의 전당 : 김예진과 식사권 / 편의점 상품권 2만원권" },
-        { condition: (score) => score >= 95,   name: "🎁 예진학 박사 : 김예진과 커피권 / 편의점 상품권 1만원권" },
-        { condition: (score) => score >= 70,   name: "🎁 예진학 석사 : 폴바셋 룽고 1잔" },
-        { condition: (score) => score >= 40,   name: "🎁 예진학 학사 : 메로나 1개" },
-        { condition: (score) => score >= 5,    name: "🤍 특별 참가상 : 비타500 1병" },
-        { condition: (score) => true,          name: "어쩜 이러실 수 있어요 : 김예진과 강제 커피권" } // Default
+        { condition: (score) => score === 100, name: "👑 명예의 전당 - 김예진과 식사권 / 편의점 상품권 2만원권" },
+        { condition: (score) => score >= 95,   name: "🎁 예진학 박사 - 김예진과 커피권 / 편의점 상품권 1만원권" },
+        { condition: (score) => score >= 70,   name: "🎁 예진학 석사 - 폴바셋 룽고 1잔" },
+        { condition: (score) => score >= 40,   name: "🎁 예진학 학사 - 메로나 1개" },
+        { condition: (score) => score >= 5,    name: "🤍 특별 참가상 - 비타500 1병" },
+        { condition: (score) => true,          name: "어쩜 이러실 수 있어요 - 김예진과 강제 커피권" } // Default
     ],
 
     /**
